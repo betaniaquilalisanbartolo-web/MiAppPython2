139d041fb9c79bce862622da669cd635169d47fe
betaniaquilalisanbartolo-web <betaniaquilalisanbartolo@gmail.com>
2026-08-23 03:52:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ elif menu == "📋 Reportes de Cultos de Célula":
 
 
     # ================= PANEL DE CONTROL =================
-    elif menu == "📊 Panel de Control y Reportes":
+elif menu == "📊 Panel de Control y Reportes":
         st.subheader("📊 Panel de Análisis Automático de la Iglesia")
         conn = sqlite3.connect(DB_PATH)
         df_cell = pd.read_sql_query("SELECT * FROM cell_reports", conn)
