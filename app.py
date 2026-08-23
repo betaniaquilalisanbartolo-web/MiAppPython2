5b3c213610b69dce431421c489dca01e78400657
betaniaquilalisanbartolo-web <betaniaquilalisanbartolo@gmail.com>
2026-08-23 03:17:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ if st.session_state['logged_in']:
         anio_actual = hoy.year
 
         # --- KPIs básicos ---
-                            # --- Gráfica de crecimiento por célula ---
+        # --- Gráfica de crecimiento por célula ---
         st.markdown("### 📈 Crecimiento de las Células (Miembros, Convertidos y Asistencia)")
         if not df_members.empty or not df_converts.empty or not df_cell_mes.empty:
             # Miembros por célula
