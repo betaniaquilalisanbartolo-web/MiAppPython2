d06921d78ba7ca7c67985e56f4ced71c2da360d8
betaniaquilalisanbartolo-web <betaniaquilalisanbartolo@gmail.com>
2026-08-25 00:21:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -249,12 +249,12 @@ else:
                 conn.close()
                 st.success(f"Descarriado {full_name} registrado en la célula {cell}")
     # --- Administración ---
-with tab5:
-    st.subheader("⚙️ Administración")
+    with tab5:
+      st.subheader("⚙️ Administración")
 
-    st.markdown("### 🌱 Registrar nueva célula y líder")
-    cell_name = st.text_input("Nombre de la célula")
-    leader = st.text_input("Nombre del líder")
+       st.markdown("### 🌱 Registrar nueva célula y líder")
+     cell_name = st.text_input("Nombre de la célula")
+      leader = st.text_input("Nombre del líder")
     if st.button("Registrar célula"):
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
