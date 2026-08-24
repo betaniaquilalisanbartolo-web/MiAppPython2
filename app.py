f0d680b6e9da734f09bba724ae67d1147e1eee4b
betaniaquilalisanbartolo-web <betaniaquilalisanbartolo@gmail.com>
2026-08-24 23:33:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,9 @@ else:
         "⚙️ Administración",
         "📊 Panel"
     ])
+    
 # --- Miembros ---
- with tab1:
+with tab1:
         st.subheader("Registro de Miembros")
         conn = sqlite3.connect(DB_PATH)
         cells = pd.read_sql_query("SELECT cell_name FROM cells", conn)
