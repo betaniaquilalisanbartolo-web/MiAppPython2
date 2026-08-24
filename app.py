88c71f992c1386bdbc3b7a6495d2481fd08a73fd
betaniaquilalisanbartolo-web <betaniaquilalisanbartolo@gmail.com>
2026-08-24 19:35:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ else:
                 conn.close()
                 st.success(f"Descarriado {full_name} registrado en la célula {cell}")
                   # --- Administración ---
-    with tab6:
+    with tab5:
      st.subheader("⚙️ Administración")
 
     st.markdown("### 🌱 Registrar nueva célula y líder")
@@ -269,7 +269,7 @@ else:
 
 
     # --- Panel ---
-    with tab5:
+    with tab6:
      st.subheader("Panel de Control y Gráficas")
 
     # Conexión y carga de datos
