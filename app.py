8d48569f292fac2e6f1c4ae2541640920ae3847c
betaniaquilalisanbartolo-web <betaniaquilalisanbartolo@gmail.com>
2026-08-24 23:08:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ with tab5:
         st.subheader("⚙️ Administración")
 
         st.markdown("### 🌱 Registrar nueva célula y líder")
-        cell_name = st.text_input("Nombre de la célula")
+    cell_name = st.text_input("Nombre de la célula")
     leader = st.text_input("Nombre del líder")
 
     if st.button("Registrar célula"):
